4daeda27cd8a4301dc961d29ec3d084f406daabf
Krishnan Mani <km@krishnanm.com>
2015-10-19 17:08:29 +0530

    Modified the script to print regions to accept a profile name argument

--- utility/list/printRegions.sh
+++ utility/list/printRegions.sh
@@ -2,4 +2,13 @@
 
 # Prints a list of all regions currently available for EC2
 
-aws ec2 describe-regions | awk '{ print $3 }'
+export SCRIPT_NAME="$0"
+
+if [ $# -ne 1 ]
+then
+	echo "Missing profile as solitary argument"
+	echo "Usage: $SCRIPT_NAME profile"
+	exit 1
+fi
+
+aws --profile $1 ec2 describe-regions | awk '{ print $3 }' | sort
