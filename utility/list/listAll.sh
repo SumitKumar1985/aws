6e6356cad51a15cfc8145df261d0b66c1e5db465
Krishnan Mani <km@krishnanm.com>
2015-10-19 17:23:24 +0530

    Added omnibus script

---
+++ utility/list/listAll.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export SCRIPT_NAME="$0"
+
+if [ $# -ne 1 ]
+then
+	echo "Missing profile as solitary argument"
+	echo "Usage: $SCRIPT_NAME profile"
+	exit 1
+fi
+
+echo "Listing EC2 hosts"
+source listHosts.sh $1
+
+echo "Listing Elastic IPs"
+source listElasticIPs.sh $1
+
+echo "Listing CloudWatch alarms"
+source listAlarmNames.sh $1
+
+echo "Listing EBS volumes"
+source listVolumes.sh $1
+
+echo "Listing AMIs"
+source listAMIs.sh $1
+
+echo "Listing EBS snapshots"
+source listSnapshots.sh $1
