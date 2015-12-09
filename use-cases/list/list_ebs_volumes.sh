bc9c54dc39db0bcedd2d1718b0e3330a4a85ca5f
Krishnan Mani <km@krishnanm.com>
2015-12-09 06:06:21 +0530

    Updated the script to list EBS volumes across all regions

--- use-cases/list/list_ebs_volumes.sh
+++ use-cases/list/list_ebs_volumes.sh
@@ -7,17 +7,19 @@ export SCRIPT_NAME="$0"
 
 if [ $# -ne 1 ]
 then
-	echo "Missing profile as solitary argument"
+	echo "Error: missing profile argument"
 	echo "Usage: $SCRIPT_NAME profile"
 	exit 1
 fi
 
 PROFILE="$1"
 
-ALL_REGIONS=$(aws --profile ${PROFILE} --output text ec2 describe-regions | awk '{ print $NF }' | sort)
+ALL_REGIONS=$(aws --profile ${PROFILE} --output text ec2 describe-regions |\
+								awk '{ print $NF }' | sort)
 
 for region in ${ALL_REGIONS}
-do 
-	echo "Region: $region"
-    aws --profile ${PROFILE} --region ${region} --output text ec2 describe-volumes | grep 'VOLUMES' | awk '{ print $(NF-1) }'
+do
+	echo "Region: ${region}"
+    aws --profile ${PROFILE} --region ${region} --output text ec2 describe-volumes |\
+				grep 'VOLUMES' | awk '{ print $(NF-1) }'
 done
