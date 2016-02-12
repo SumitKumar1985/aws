7fefbedda8c633d26189ba4f6244a312f3ad6a7f
Krishnan Mani <km@krishnanm.com>
2016-02-12 15:03:33 +0530

    Cured listing EBS volumes

--- use-cases/list/list_ebs_volumes.sh
+++ use-cases/list/list_ebs_volumes.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 # Use this script to look for all volumes in all available AWS regions and print them to the console
-# This expects the name of the profile to use with the aws-cli as the only argument to the script
 
 export SCRIPT_NAME="$0"
 
-if [ $# -ne 1 ]
+if [ -z "$AWS_PROFILE" ]
 then
-	echo "Error: missing profile argument"
-	echo "Usage: $SCRIPT_NAME profile"
+	echo "Missing AWS_PROFILE: Set AWS_PROFILE to the user credential profile to use with aws-cli"
+	echo "Usage: $SCRIPT_NAME"
 	exit 1
 fi
 
-PROFILE="$1"
-
-ALL_REGIONS=$(aws --profile ${PROFILE} --output text ec2 describe-regions |\
-								awk '{ print $NF }' | sort)
+ALL_REGIONS=$(aws --output text ec2 describe-regions | awk '{ print $NF }' | sort)
 
 for region in ${ALL_REGIONS}
 do
-	echo "Region: ${region}"
-    aws --profile ${PROFILE} --region ${region} --output text ec2 describe-volumes |\
-				grep 'VOLUMES' | awk '{ print $(NF-1) }'
+		echo "Region: ${region}"
+    aws ec2 describe-volumes \
+				--region ${region} \
+				--output text \
+				--query 'Volumes[*].[VolumeId]'
 done
