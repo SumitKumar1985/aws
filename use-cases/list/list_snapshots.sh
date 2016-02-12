0121c9dbcfc13ede21622474a22a25595da0bab1
Krishnan Mani <km@krishnanm.com>
2016-02-12 15:19:00 +0530

    Cured listing snapshots

--- use-cases/list/list_snapshots.sh
+++ use-cases/list/list_snapshots.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 # Use this script to look for all snapshots owned by the account in all available AWS regions and print them to the console
-# This expects the name of the profile to use with the aws-cli as the only argument to the script
 
 export SCRIPT_NAME="$0"
 
-if [ $# -ne 1 ]
+if [ -z "$AWS_PROFILE" ]
 then
-	echo "Missing profile as solitary argument"
-	echo "Usage: $SCRIPT_NAME profile"
-        exit 1
+	echo "Missing AWS_PROFILE: Set AWS_PROFILE to the user credential profile to use with aws-cli"
+	echo "Usage: $SCRIPT_NAME"
+	exit 1
 fi
 
-PROFILE="$1"
-
-ALL_REGIONS=$(aws --profile ${PROFILE} --output text ec2 describe-regions |\
-								awk '{ print $NF }' | sort)
+ALL_REGIONS=$(aws --output text ec2 describe-regions | awk '{ print $NF }' | sort)
 
 for region in ${ALL_REGIONS}
 do
 	echo "Region: $region"
-  aws --profile ${PROFILE} --region ${region} ec2 describe-snapshots \
-			--owner self --output text | awk '{ print $(NF-4) }'
+  aws ec2 describe-snapshots \
+	 		--owner self \
+			--region ${region} \
+			--output text \
+			--query 'Snapshots[*].[SnapshotId]'
 done
